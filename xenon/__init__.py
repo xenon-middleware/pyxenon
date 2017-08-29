328337652a32aace2551dd6ae4fe1d287dfaf605
Johan Hidding <johannes.hidding@gmail.com>
2017-08-29 15:05:51 +0200

    export some more message types

--- xenon/__init__.py
+++ xenon/__init__.py
@@ -1,9 +1,19 @@
 from .server import (Server)
-from .objects import (FileSystem, Scheduler, Path)
+
+from .objects import (
+    FileSystem, Scheduler, Path,
+    PosixFilePermission)
+
 from .proto.xenon_pb2 import (
-    JobDescription, CopyRequest)
+    JobDescription, CopyRequest, QueueStatus, JobStatus,
+    CopyStatus, CertificateCredential, PasswordCredential,
+    PropertyDescription)
 
 __version__ = "2.0.0-alpha"
 __all__ = [
-    'Server', 'FileSystem', 'Scheduler', 'Path',
-    'JobDescription', 'CopyRequest']
+    'Server',
+    'FileSystem', 'Scheduler', 'Path',
+    'PosixFilePermission',
+    'JobDescription', 'CopyRequest', 'QueueStatus', 'JobStatus',
+    'CopyStatus', 'CertificateCredential', 'PasswordCredential',
+    'PropertyDescription']
