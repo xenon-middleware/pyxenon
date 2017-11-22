4aa76bb23515e1321b02ba3b64d0321855d384de
Johan Hidding <johannes.hidding@gmail.com>
2017-11-22 14:28:52 +0100

    add unittest for PathAlreadyExistsException

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,6 +1,14 @@
-from xenon import (FileSystem)
+import pytest
+from xenon import (FileSystem, Path, PathAlreadyExistsException)
 
 
 def test_path_separator(xenon_server, tmpdir):
     with FileSystem.create(adaptor='file') as filesystem:
         assert filesystem.get_path_separator() == '/'
+
+
+def test_path_already_exists_error(xenon_server, tmpdir):
+    with FileSystem.create(adaptor='file') as filesystem:
+        with pytest.raises(PathAlreadyExistsException):
+            filesystem.create_directory(Path('test'))
+            filesystem.create_directory(Path('test'))
